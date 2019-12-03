56a64552410ade1d5cbd7d156c4cc68097b92241
shady-abdelhamid <shady.abdelhamid@outlook.com>
2019-12-03 09:57:53 +0200

    add log for incomming url path

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(req, res){
+    var parsedUrl = url.parse(req.url, true);
+
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, ''); 
     res.end('Hello World\n');
+
+    console.log(`request recieved on path: ${trimmedPath}`);
 });
 
 server.listen(3000, function(){
